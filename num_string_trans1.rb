863aaeaca9e14eeca9c46b1ca17ea779e9321f70
Zen <innovationlife.net@gmail.com>
2015-07-24 18:55:11 +0900

    Add num_string_trans.rb

---
+++ num_string_trans1.rb
@@ -0,0 +1,5 @@
+puts "性を入力してください。"
+family_name = gets.chomp
+puts "名を入力してください。"
+first_name = gets.chomp
+puts "こんにちは、" + family_name + first_name + "さん"
---
+++ num_string_trans2.rb
@@ -0,0 +1,4 @@
+puts "何枚の諭吉さんがほしいですか？"
+yukiti = gets
+yukiti = yukiti.to_i + 1
+puts yukiti.to_s + "枚のほうがいいのでは？"
