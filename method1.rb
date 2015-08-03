70406fb80f9410f5a874898c05faa47488b8f8f7
Zen <innovationlife.net@gmail.com>
2015-08-03 17:13:50 +0900

    Add method files

---
+++ method1.rb
@@ -0,0 +1,3 @@
+puts "何が望みだ？"
+wish = gets.chomp
+puts "なにぃ? " + wish + "だとー!! おまえは首だ!!"
---
+++ method2.rb
@@ -0,0 +1,5 @@
+lineWidth = 60
+puts "目次".center  lineWidth
+puts "1章:  数".ljust(lineWidth/2) + "p. 1".rjust(lineWidth/2)
+puts "2章:  文字".ljust(lineWidth/2) + "p. 72".rjust(lineWidth/2)
+puts "1章:  変数".ljust(lineWidth/2) + "p. 118".rjust(lineWidth/2)
